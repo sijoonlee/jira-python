06b4374ce1691a2bb7e97dbe5c09fc25f4971d40
Sijoon Lee <shijoonlee@gmail.com>
2020-08-12 20:02:53 -0700

    little more on pandas

--- businessLogic/metrics/storyPoints.py
+++ businessLogic/metrics/storyPoints.py
@@ -1,12 +1,13 @@
 from businessLogic.metrics.metricsFunctions import countByGroup, percentageByGroup
 from db.sqlite3.connector import SqliteConnector
 import pandas as pd
-
+import datetime
+from datetime import timezone
 dbFile = './db/sqlite3/storage/db.sqlite'
 dbConnector = SqliteConnector(dbFile)
 
 def getMetricsPerProject(projectKey):
-    selectedFields = ["Issue.key as issue", "Issue.storyPoints", "Status.name as status", "Issue.assigneeName"]
+    selectedFields = ["Issue.key as issue", "Issue.storyPoints", "IssueType.name as issueType","Status.name as status", "Issue.assigneeName", "Issue.created"]
     joinClauses = [
         {"type":"LEFT", "tableName":"IssueType", "onClause":"Issue.issueTypeId = IssueType.id"},
         {"type":"LEFT", "tableName":"Project", "onClause":"Issue.projectId = Project.id"},
@@ -17,11 +18,22 @@ def getMetricsPerProject(projectKey):
 
     # df = percentageByGroup(dbConnector.connection, statement, "issueType")
     df = pd.read_sql_query(statement, dbConnector.connection)
+
+    # parse string to datetime
+    df["created"] = pd.to_datetime(df["created"])
+
+    # filter > 2020.1.1
+    df = df[df["created"] > datetime.datetime(year=2020,month=1,day=1).replace(tzinfo=timezone.utc)]
+    
+    # drop data where story points are none
     df = df.drop(df[df.storyPoints == "None"].index)
     df["storyPoints"] = pd.to_numeric(df["storyPoints"])
 
+
     df1 = df.groupby("assigneeName").sum()
     print(df1)
+    df2 = df.groupby("issueType").sum()
+    print(df2)
     df = df.groupby("status").sum()
     
     #df = df.groupby("storyPoints").count()
