eda4e4c3f467e5ad8ad426a403364f97ab0cf2ec
Sijoon Lee <shijoonlee@gmail.com>
2020-08-19 16:15:55 -0700

    return type changed

--- app.py
+++ app.py
@@ -21,20 +21,14 @@ def getSprintBetween():
     endStr = request.args.get("end") # "2020-03-01"
     boardId = request.args.get("boardId") # 93 for omp
     df = sprint.calculateWorkDonePercentage(boardId, startStr, endStr)
-    html = df.to_html()
-    print(html)
-    response = jsonify({"state":200, "message":html})
-    print(response)
-    return response
+    return df.to_json(orient="split")
 
 @app.route('/sprintInBoard', methods=['GET'])
 @cross_origin()
 def getSprintInBoard():
     boardId = request.args.get("boardId", "*")
     df = queries.getSprintByBoardId(boardId)
-    json = df.to_json()
-    response = jsonify({"state":200, "message":json})
-    return response
+    return df.to_json(orient="index")
 
 
 @app.route('/sprint', methods=['GET'])
@@ -42,54 +36,28 @@ def getSprintInBoard():
 def getSprint():
     sprintId = request.args.get("sprintId", "*")
     df = queries.getSprintById(sprintId)
-    json = df.to_json()
-    response = jsonify({"state":200, "message":json})
-    return response
+    return df.to_json(orient="index")
 
 @app.route('/board', methods=['GET'])
 @cross_origin()
 def getBoard():
     boardId = request.args.get("boardId", "*")
     df = queries.getBoardById(boardId)
-    json = df.to_json()
-    response = jsonify({"state":200, "message":json})
-    return response
-# message looks like
-# {
-#     "id": {
-#         "0": "40",
-#         "1": "50",
-#         "2": "62"
-#     },
-#     "name": {
-#         "0": "Dev",
-#         "1": "Dev Board - Core",
-#         "2": "PM Board - OMP"
-#     },
-#     "type": {
-#         "0": "scrum",
-#         "1": "scrum",
-#         "2": "kanban"
-#     }
-# }
+    return df.to_json(orient="split")
 
 @app.route('/issue', methods=['GET'])
 @cross_origin()
 def getIssue():
     issueId = request.args.get("issueId", "*")
     df = queries.getIssueById(issueId)
-    json = df.to_json()
-    response = jsonify({"state":200, "message":json})
-    return response
+    return df.to_json(orient="split")
 
 @app.route('/issueBySprint', methods=['GET'])
 @cross_origin()
 def getIssueBySprint():
     sprintId = request.args.get("sprintId", "*")
     df = queries.getIssueBySprintId(sprintId)
-    json = df.to_json()
-    response = jsonify({"state":200, "message":json})
-    return response
+    return df.to_json(orient="split")
 
 
 @app.route('/post', methods=['POST'])
